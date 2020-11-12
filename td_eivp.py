858d07f63d188c700826bfaf53edad9a9b16aa85
Ambre <ambre.boussugeaubert@gmail.com>
2020-11-12 18:43:09 +0100

    Update td_eivp.py

--- td_eivp.py
+++ td_eivp.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import csv
 import os
 os.chdir('C:\\Users\\user\\Dropbox\\EIVP\\Programmation Python\\Projet')
@@ -42,48 +41,41 @@ def tab_donnees():
 
 
 
-=======
-import csv
-import os
-os.chdir('C:\\Users\\user\\Dropbox\\EIVP\\Programmation Python\\Projet')
 
-f = open('EIVP_KM.csv')
+#import csv
+#import os
+#os.chdir('C:\\Users\\user\\Dropbox\\EIVP\\Programmation Python\\Projet')
 
-fichier_csv = csv.reader(f)
+#f = open('EIVP_KM.csv')
 
-
-import numpy as np
-import matplotlib.pyplot as plt
+#fichier_csv = csv.reader(f)
 
 
-
-def tableau_donnees():                                                          #fichier d'essai, peut être supprimé
-    tab = []
-    for line in fichier_csv :
-        tab.append(line)
-    return tab
-
-
-def tab_donnees():
-    tab = []
-    for line in fichier_csv :
-        for i in range(len(line)):
-            tab.append(line[i].split(sep = ";", maxsplit = -1))
-    n = len(tab)
-    tab_donnees = []
-    for k in range(1, n):
-        line = []
-        for i in range(6):
-            line.append(float(tab[k][i]))
-        line.append(tab[k][6])
-        tab_donnees.append(line)
-    return tab_donnees                                                          #fonctionne
+#import numpy as np
+#import matplotlib.pyplot as plt
 
 
 
+#def tableau_donnees():                                                          #fichier d'essai, peut être supprimé
+ #   tab = []
+  #  for line in fichier_csv :
+   #     tab.append(line)
+    #return tab
 
 
+#def tab_donnees():
+ #   tab = []
+  #  for line in fichier_csv :
+    #    for i in range(len(line)):
+     #       tab.append(line[i].split(sep = ";", maxsplit = -1))
+    #n = len(tab)
+    #tab_donnees = []
+    #for k in range(1, n):
+     #   line = []
+      #  for i in range(6):
+       #     line.append(float(tab[k][i]))
+        #line.append(tab[k][6])
+        #tab_donnees.append(line)
+   # return tab_donnees                                                          #fonctionne
 
 
-
->>>>>>> efdc2349922ad24de365a07378b57fa0c9861156
