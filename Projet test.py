e6b177eaf6a6c6ae9a98dec7b7f00d06d511ac59
leeloorouch <72785767+leeloorouch@users.noreply.github.com>
2020-11-17 10:11:13 +0100

    Add files via upload

--- Projet test.py
+++ Projet test.py
@@ -3,9 +3,7 @@ import os
 import matplotlib.pyplot as plt
 os.chdir('C:\\Users\\user\\Dropbox\\EIVP\\Programmation Python\\Projet')
 
-f = open('post-32566-EIVP_KM.csv')
 
-fichier_csv = csv.reader(f)
 
 
 import numpy as np
@@ -13,59 +11,70 @@ import matplotlib.pyplot as plt
 
 
 
-def tableau_donnees():                                                          #fichier d'essai, peut être supprimé
-    tab = []
-    for line in fichier_csv :
-        tab.append(line)
-    return tab
+def tab_donnees(capteur, variable, start_date, end_date):
+    with open('post-32566-EIVP_KM.csv') as f :
+        fichier_csv = csv.reader(f)
+        var = 0
+        if variable == 'noise' :
+            var = 1
+        elif variable == 'temp':
+            var = 2
+        elif variable == 'humidity':
+            var = 3
+        elif variable == 'lum':
+            var = 4
+        else :
+            var = 5
+        list = []
+        for line in fichier_csv :
+            for i in range(len(line)):
+                list.append(line[i].split(sep = ";", maxsplit = -1))
+        n = len(list)
+        list_var_capteur = []
+        list_dates_capteur = []
+        for k in range(1, n):
+            date = []
+            if ((float(list[k][1])) == capteur) :
+                list_var_capteur.append(float(list[k][var]))
+                date = list[k][7][0:19]
+                list_dates_capteur.append(date)
+        dates_intervalle = []
+        var_intervalle = []
+        for i in range(len(list_dates_capteur)):
+            if (start_date < list_dates_capteur[i] < end_date):
+                dates_intervalle.append(list_dates_capteur[i])
+                var_intervalle.append(list_var_capteur[i])
 
-
-def tab1_donnees():
-    tab = []
-    for line in fichier_csv :
-        for i in range(len(line)):
-            tab.append(line[i].split(sep = ";", maxsplit = -1))
-    n = len(tab)
-    tab_donnees = []
-    tab_dates = []
-    for k in range(1, n):
-        line = []
-        for i in range(1, 7):
-            line.append(float(tab[k][i]))
-        line.append(tab[k][7])
-        tab_donnees.append(line)
-    return tab_donnees                                                          #fonctionne mais on ne peut pas en faire un tab numpy car le dtype de la colonne 7 est un string, et un tab numpy ne prend qu'un seul dtype
-
-
-
-
-
-def tab_donnees(capteur):                                                       #fonctionne. Il faut maintenant faire pareil pour chaque capteur, donc 6 tableaux de données et 6 tableaux de dates
-    list = []
-    for line in fichier_csv :
-        for i in range(len(line)):
-            list.append(line[i].split(sep = ";", maxsplit = -1))
-    n = len(list)
-    list_donnees = []
-    list_dates = []
-    for k in range(1, n):
-        if (float(list[k][1])) == capteur :
-            line = []
-            for i in range(2, 7):
-                line.append(float(list[k][i]))
-            date = list[k][7][0:19]
-            list_donnees.append(line)
-            list_dates.append(date)
-    tab_donnees = np.array(list_donnees)
-    tab_donnees = np.array(list_donnees)
-
-    temp_11 = []
-    dates_1 = []
-    for i in range(len(list_dates)):
-        if ('2019-08-14' < list_dates[i] < '2019-08-23'):
-            dates_1.append(list_dates[i])
-            temp_11.append(tab_donnees[i,0])
-
-
-    plt.plot(dates_1, temp_11)
+    plt.plot(dates_intervalle, var_intervalle)
     plt.show()
+    return (dates_intervalle, var_intervalle)
+
+
+
+def courbe_stat(capteur, variable, start_date, end_date):
+    l = tab_donnees(capteur, variable, start_date, end_date)
+    n = len(l)
+    dates = l[0]
+    var = l[1]
+    s = 0
+    moy = 0
+    sigma = 0
+    min = var[0]
+    max = var[0]
+
+    for k in range(n):
+        s = s+var[k]
+        if (var[k] <  min):
+            min = var[k]
+        else :
+            max = var[k]
+    moy = s/n
+    for i in range(n):
+        ecart_moy = ecart_moy + (var[i]-moy)*(var[i]-moy)
+    sigma = ecart_moy/n
+
+    plt.plot(dates, var)
+    plt.show()
+
+
+
